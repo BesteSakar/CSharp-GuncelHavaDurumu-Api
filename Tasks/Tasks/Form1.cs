165df81c0e08a38d5866baae380db7e70c1a22a6
BesteSakar <bestesakarr@outlook.com>
2022-04-19 21:47:47 +0300

    Api ile güncel şehirlere göre hava durumunu gösterme.
     Api ile sehirlere göre güncel hava durumunu datagrid ile gösteren bilgileri getirme..

---
+++ Tasks/Tasks/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Tasks
+{
+    public partial class Form1 : Form
+    {
+
+
+        string hava_durumu = "https://mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_getir_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(hava_durumu);
+            XmlElement root = doc1.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("sehirler");
+
+            foreach(XmlNode node in nodes)
+            {
+                string ili = node["ili"].InnerText;
+                string durum = node["Durum"].InnerText;
+                string maks_sıcaklık = node["Mak"].InnerText;
+
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = ili;
+                row.Cells[1].Value = durum;
+                row.Cells[2].Value = maks_sıcaklık;
+
+                dataGridView1.Rows.Add(row);
+      
+
+            }
+        }
+    }
+}
